145c229792916e872eb4f84131f51f62f257fc4d
RC04600 <RC04600@HYD6063>
2018-06-24 01:22:11 +0530

    Report

--- Leader Billing/ReportByChart.Designer.cs
+++ Leader Billing/ReportByChart.Designer.cs
@@ -29,37 +29,63 @@
         private void InitializeComponent()
         {
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.gvAmtDetails = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gvAmtDetails)).BeginInit();
             this.SuspendLayout();
             // 
             // chart1
             // 
             chartArea1.Name = "ChartArea1";
             this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(33, 73);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
             this.chart1.Name = "chart1";
             series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
+            series1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            series1.IsValueShownAsLabel = true;
+            series1.LabelForeColor = System.Drawing.Color.Red;
+            series1.Name = "Balance Amount";
+            series1.SmartLabelStyle.CalloutLineColor = System.Drawing.Color.Red;
+            series1.ToolTip = "#VAL";
+            series1.YValuesPerPoint = 2;
             this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(1013, 376);
+            this.chart1.Size = new System.Drawing.Size(827, 516);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
+            // 
+            // gvAmtDetails
+            // 
+            this.gvAmtDetails.AllowUserToAddRows = false;
+            this.gvAmtDetails.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.gvAmtDetails.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.gvAmtDetails.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gvAmtDetails.Location = new System.Drawing.Point(845, 12);
+            this.gvAmtDetails.Name = "gvAmtDetails";
+            this.gvAmtDetails.ReadOnly = true;
+            this.gvAmtDetails.Size = new System.Drawing.Size(378, 516);
+            this.gvAmtDetails.TabIndex = 204;
             // 
             // ReportByChart
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1058, 484);
+            this.ClientSize = new System.Drawing.Size(1235, 551);
+            this.Controls.Add(this.gvAmtDetails);
             this.Controls.Add(this.chart1);
             this.Name = "ReportByChart";
             this.Text = "ReportByChart";
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gvAmtDetails)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -67,5 +93,6 @@
         #endregion
 
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataGridView gvAmtDetails;
     }
 }
--- Leader Billing/ReportByChart.cs
+++ Leader Billing/ReportByChart.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,30 @@ namespace Leader
 {
     public partial class ReportByChart : Form
     {
+        SqlConnection dataConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["LeaderConn"].ConnectionString);
+
         public ReportByChart()
         {
             InitializeComponent();
+            DataTable dt = LoadOutStandingADDetails(); ;
+            chart1.DataSource = dt;
+            chart1.Series["Balance Amount"].XValueMember = "CommissionAgent";
+            chart1.Series["Balance Amount"].YValueMembers = "Balance Amount";
+            //chart1.Series["Total Amount"].YValueMembers = "TotalAmount";
+            chart1.Titles.Add("Balance Amount By Commission Agent");
+            chart1.Series["Balance Amount"]["PixelPointWidth"] = "40";
+            gvAmtDetails.DataSource = dt;
+            gvAmtDetails.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.ColumnHeader;
+        }
+
+        public DataTable LoadOutStandingADDetails()
+        {
+            string strCmd = "spGetOutstandingADs_CommissionAgentChart";
+            SqlDataAdapter da = new SqlDataAdapter(strCmd, dataConnection);
+            da.SelectCommand.CommandType = CommandType.StoredProcedure;
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
         }
     }
 }
